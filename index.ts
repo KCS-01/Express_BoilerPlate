7a406065dca10ce7c9bf526d9048a77b23e510cd
KCS-01 <ckstn510@naver.com>
2024-07-20 17:37:31 +0900

    Feat: README에 작동 순서 추가

--- index.ts
+++ index.ts
@@ -3,12 +3,12 @@ import config from "config";
 import middlewares from "middlewares";
 import App from "./app";
 
-function bootstrap(port: number | string = 3005, server: () => Express) {
-	// 인스턴스 생성
+const bootstrap = (port: number | string = 3005, server: () => Express) => {
+	// 서버 인스턴스 생성
 	const app = new App(port, server);
 
 	// 미들웨어 세팅
 	app.setMiddleWares(middlewares);
-}
+};
 
 bootstrap(config.LISTEN_PORT, express);
